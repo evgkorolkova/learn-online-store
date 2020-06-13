12886f25356ed977fd15167720e43b5772159edc
evgkorolkova <ev.korolkova73@gmail.com>
2020-06-13 11:05:42 +0400

    Добавила модель писатель, verbose name и default значения

--- shop/models.py
+++ shop/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 
 class Writer(models.Model):
     name = models.CharField("Автор", max_length=250)
-    description = models.TextField("Информация об авторе", blank=True, default="")
+    description = models.TextField("Информация об авторе", blank=True)
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
@@ -22,15 +22,13 @@ class Product(models.Model):
     title = models.CharField("Книга", max_length=50)
     description = models.TextField("Описание книги", blank=True)
     category = models.ManyToManyField(Category, verbose_name="Категория", related_name="category_product")
-    writer = models.ManyToManyField(Writer, verbose_name="Писатель", related_name="writer_product")
+    writer = models.ManyToManyField(Writer, verbose_name="Автор", related_name="writer_product")
     url = models.SlugField(max_length=200, unique=True)
     sku = models.CharField("Артикул", max_length=10, unique=True)
     date_create = models.DateField("Дата создания")
-    price = models.DecimalField("Цена", decimal_places=2, max_digits=5)
+    price = models.DecimalField("Цена", decimal_places=2, max_digits=5, default=0)
     stock = models.PosiiveIntegerField("Количество на складе", default=1)
     status = models.CharField("Статус на сайте", max_length=20, choices=STATUS_CHOICES, default="Enable")
     class Meta:
         verbose_name = "Книга"
         verbose_name_plural = "Книги"
-
-
