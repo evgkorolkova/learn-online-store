d1c3e770acc338e1e2e6cc854466e304f454a306
Quderlo <4546436@gmail.com>
2020-07-08 00:33:37 +0500

    add views and templates

--- shop/models.py
+++ shop/models.py
@@ -1,45 +1,103 @@
+from datetime import datetime
+from django.contrib.auth.models import User
 from django.db import models
-from datetime import date, datetime
+from django.utils import timezone
+from django.conf import settings
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        related_name='profile',
+        default=1,
+        on_delete=models.PROTECT
+    )
+
+
+def create_user_callback(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+
+class Order(models.Model):
+    delivery_address = models.CharField(
+        verbose_name='Адрес и дом',
+        max_length=128,
+        default=None,
+        blank=True,
+        null=True
+    )
+    delivery_address_house_part = models.CharField(
+        verbose_name='Подъезд',
+        max_length=128,
+        default=None,
+        blank=True,
+        null=True
+    )    
+    delivery_apart = models.CharField(
+        verbose_name='Квартира / Офис',
+        max_length=128,
+        default=None,
+        blank=True,
+        null=True
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+    phone_number = models.CharField(
+        verbose_name='Номер телефона',
+        max_length=15,
+        default=None,
+        null=True,
+        blank=False
+    )
+    order_number = models.IntegerField() #Нужен ли? Можно использовать для хранение трек номера посылки          
+
 
 class Category(models.Model):
     title = models.CharField("Категория", max_length=50)
     description = models.TextField("Описание категории", blank=True, null=True)
     url = models.SlugField(max_length=200, unique=True)
+    
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+    
     def __str__(self):
         return self.title
+
 
 
 class Writer(models.Model):
     name = models.CharField("Автор", max_length=250)
     description = models.TextField("Информация об авторе", blank=True, null=True)
+    
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
+    
     def __str__(self):
-        return self.name
+    	return self.name
 
 
 class Product(models.Model):
-    STATUS_CHOICES = (
-        ("yes", "Enable"),
-        ("no", "Disable")
+	STATUS_CHOICES = (
+		("yes", "Enable"),
+        ("no", "Disable") 	
         )
-    title = models.CharField("Книга", max_length=50)
-    description = models.TextField("Описание книги", blank=True, null=True)
-    category = models.ManyToManyField(Category, verbose_name="Категория", related_name="category_product")
-    writer = models.ForeignKey(Writer, verbose_name="Автор", related_name="writer_product", on_delete=models.SET_NULL, null=True)
-    url = models.SlugField(max_length=200, unique=True)
-    sku = models.CharField("Артикул", max_length=10, unique=True)
-    date_create = models.DateTimeField("Дата создания", auto_now_add=True, null=True)
-    price = models.DecimalField("Цена", decimal_places=2, max_digits=5, default=0)
-    stock = models.PositiveIntegerField("Количество на складе", default=1)
-    status = models.CharField("Статус на сайте", max_length=20, choices=STATUS_CHOICES, default="yes")
-    class Meta:
-        verbose_name = "Книга"
-        verbose_name_plural = "Книги"       
-    def __str__(self):
-        return self.title
-    
+
+	title = models.CharField("Книга", max_length=50)
+	description = models.TextField("Описание книги", blank=True, null=True)
+	category = models.ManyToManyField(Category, verbose_name="Категория", related_name="category_product")
+	writer = models.ForeignKey(Writer, verbose_name="Автор", related_name="writer_product", on_delete=models.SET_NULL, null=True)
+	url = models.SlugField(max_length=200, unique=True)
+	sku = models.CharField("Артикул", max_length=10, unique=True)
+	date_create = models.DateTimeField("Дата создания", auto_now_add=True, null=True)
+	price = models.DecimalField("Цена", decimal_places=2, max_digits=5, default=0)
+	stock = models.PositiveIntegerField("Количество на складе", default=1)
+	status = models.CharField("Статус на сайте", max_length=20, choices=STATUS_CHOICES, default="yes")
+
+	class Meta:
+		verbose_name = "Книга"
+		verbose_name_plural = "Книги"
+
+	def __str__(self):
+		return self.title
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Order
+from .models import Category
+from .models import Writer
+from .models import Profile
+from .models import Product 	
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 # Create your views here.
+
+def product_list(request):
+	products = Product.objects.filter(date_create__lte=timezone.now()).order_by('date_create')
+	return render(request, 'shop/product_list.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'shop/product_detail.html', {'product': product})
